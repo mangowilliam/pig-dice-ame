dfb4fee21b7810b32e74d9c267c9e5bb3b51fad6
mangowilliam <williammango2015@gmail.com>
2019-05-11 10:52:21 +0300

    add variables global

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var scores, totalScore, playingPlayer, gaming;
+var btnRoll = document.querySelector("btn-roll");
+var btnHold = document.querySelector("btn-hold");
+var btnNew = document.querySelector("btn-new");
