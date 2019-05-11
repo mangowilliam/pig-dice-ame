075ff95917613dff4960eb02ecad173aa1db66c7
mangowilliam <williammango2015@gmail.com>
2019-05-11 13:50:04 +0300

    add functions and arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
-var scores, totalScore, playingPlayer, gaming;
-var btnRoll = document.querySelector("btn-roll");
-var btnHold = document.querySelector("btn-hold");
-var btnNew = document.querySelector("btn-new");
+function gamer(gamerId) {
+  this.score = 0;
+  this.gamer1CurrentTotalScore = 0;
+  this.turn = 0;
+  this.gamerId = gamerId;
+}
 
-var diceImageSides = {
-  side1: "img/die1.jpg",
-  side2: "img/die2.jpg",
-  side3: "img/die3.jpg",
-  side4: "img/die4.jpg",
-  side5: "img/die5.jpg",
-  side6: "img/die6.jpg",
-};
-init();
+function answer() {
+  this.gamers = [];
+  this.winner = "";
+  this.over = 0;
+  this.dice = 0;
+}
+answer.prototype.switchGamers = function() {
+  if (this.gamers[0].turn === 1) {
+    this.gamers[0].turn = 0;
+    this.gamers[1].turn = 1;
+
+  } else if (this.gamers[1].turn === 1) {
+    this.gamers[0].turn = 1;
+    this.gamers[1].turn = 0;
+  }
+}
+}
