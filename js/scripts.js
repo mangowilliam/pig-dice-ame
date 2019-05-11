97335e8fb2dcce358f0732c024333a5d1d9b5b7e
mangowilliam <williammango2015@gmail.com>
2019-05-11 11:09:28 +0300

    add dice side object and properties

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@ var scores, totalScore, playingPlayer, gaming;
 var btnRoll = document.querySelector("btn-roll");
 var btnHold = document.querySelector("btn-hold");
 var btnNew = document.querySelector("btn-new");
+
+var diceImageSides = {
+  side1: "img/die1.jpg",
+  side2: "img/die2.jpg",
+  side3: "img/die3.jpg",
+  side4: "img/die4.jpg",
+  side5: "img/die5.jpg",
+  side6: "img/die6.jpg",
+};
+init();
