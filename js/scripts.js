7004dcdc6e26e57b5efb7a866db7ba0055d958f9
mangowilliam <williammango2015@gmail.com>
2019-05-12 15:26:19 +0300

    update index.html

--- js/scripts.js
+++ js/scripts.js
@@ -96,21 +96,91 @@ function showDice(dice) {
   if (dice >= 1) {
     $("img").attr("src", "img/" + dice + ".png").hide().fadeIn();
   }
+  $(document).ready(function() {
+    $("#play").click(function() {
+      $(".row").show();
+      j
+    });
+  });
 
 }
 $(document).ready(function() {
-      var gameChoice = $('input:radio[name=gameChoice]:checked').val();
-      var player1 = new gamer("mister");
-      var player2 = new gamer(gameChoice);
-      var totalAnswer = new answer();
+  var gameChoice = $('input:radio[name=gameChoice]:checked').val();
+  var player1 = new gamer("mister");
+  var player2 = new gamer(gameChoice);
+  var totalAnswer = new answer();
 
 
-      totalAnswer.gamer.push(gamer1, gamer2);
-      totalAnswer.pickgamer();
+  totalAnswer.gamer.push(gamer1, gamer2);
+  totalAnswer.pickgamer();
+
+  $("#gameChoice").click(function() {
+    gameChoice = $('input:radio[name=gameChoice]:checked').val();
+    if (gameChoice === "ai") {
+      totalAnswer.gamers[1].gamerId = "ai";
+    }
+
+    $(this).parent().parent().fadeOut(700, function() {
+      $("#twogamer").fadeIn();
+    });
+  });
+
+  setInterval(function() {
+      if (gamer2.gamerId === "ai" && gamer2.turn === 1) {
+        if (gamer2.gamerCurrentTotalScore <= 18) {
+          totalAnswer.throw();
+          $(".hold").hide();
+          $(".roll").hide();
+        } else {
+          totalAnswer.hold();
+          $(".hold").fadeIn();
+          $(".roll").fadeIn();
+        }
+        if (gamer2.turn === 0) {
+          $(".hold").fadeIn();
+          $(".roll").fadeIn();
+        }
+        totalAnswer.checkGame();;
+        if (totalAnswer.over === 1) {
+          $(".winner").show().text(totalAnswer.winnwer + "you won zzzz");
+
+        }
+        showDice(totalAnswer.dice);
+        showScore(gamer1, gamer2);
+        switchClass(gamer1, gamer2);
+      }
+    },
+    900);
+
+  $(".roll").click(function() {
+    totalAnswer.checkGame();
+    totalAnswer.throw();
 
 
+    showDice(totalAnswer.dice);
+    showScore(gamer1, gamer2);
+    switchClass(gamer1, gamer2);
 
-      $("#gameChoice").click(function() {
-            gameChoice = $('input:radio[name=gameChoice]:checked').val();
-            if (gameChoice === "ai") {
-              toelTheReferee.players[1].playerID = "ai";
+    if (totalAnswer.over === 1) {
+      $(".winner").show().text(totalAnswer.winner + "you wons zzzz");
+    }
+
+
+  });
+
+  // press hold to pass section
+
+  $(".hold").click(function() {
+
+    totalAnswer.hold();
+    showScore(gamer1, gamer2);
+    totalAnswer.checkGame();
+
+    switchClass(gamer1, gamer2);
+    showDice(gamer1, gamer2);
+
+    if (toelTheReferee.gameover === 1) {
+      $(".winner").show().text(totalAnswer.winner + "you won zzz");
+    }
+  });
+});
