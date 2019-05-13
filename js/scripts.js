6241617621f22e2d47d2568c90c7deb0196b639f
mangowilliam <williammango2015@gmail.com>
2019-05-13 11:10:28 +0300

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -53,15 +53,14 @@ $(document).ready(function() {
     $(".gamer2TotalScore").empty();
   });
   $(".btn-roll1").click(function(event) {
-
     $(".gamer1Score").text(gamer1.froll);
     gamer1.score = diceRandom;
     $(".gamer1OngoingScore").text(gamer1.score);
-    gamer1.froll();
+    gamer1.ongoingScore = (diceRandom += gamer1.ongoingScore);
     $(".gamer1TotalScore").text(gamer1.OngoingScore);
   });
 
-  $("button.btn-roll2").click(function(event) {
+  $(".btn-roll2").click(function(event) {
     $(".gamer2Score").text(gamer1.froll);
     gamer2.score = diceRandom;
     $(".gamer2OngoingScore").text(gamer2.score);
@@ -69,7 +68,7 @@ $(document).ready(function() {
     $(".gamer2TotalScore").text(gamer2.OngoingScore);
   });
 
-  $("button.btn-hold1").click(function(event) {
+  $(".btn-hold1").click(function(event) {
     gamer1.hold();
     $(".gamer1TotalScore").text(gamer1.totalScore);
     $(".gamer1OngoingScore").empty();
@@ -77,7 +76,7 @@ $(document).ready(function() {
     gamer1.checkWinner();
   });
 
-  $("button.btn-hold2").click(function(event) {
+  $(".btn-hold2").click(function(event) {
     gamer2.hold();
     $(".gamer2TotalScore").text(gamer2.totalScore);
     $(".gamer2OngoingScore").empty();
